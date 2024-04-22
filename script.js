6b3bdcede9652b0a5eec06c6f029786c7d5042e9
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-04-22 23:40:13 +0530

    add: implement the dom

--- script.js
+++ script.js
@@ -26,14 +26,83 @@ const gameBoard = (function() {
   };
 })();
 
+const domController = (function() {
+  const cells = document.querySelectorAll(".board > div");
+  const restartButton = document.querySelector(".restart");
+  const playerDivs = document.querySelectorAll("[class^='player']");
+
+  const updateCell = (index, marker) => {
+    if (cells[index].textContent === "") {
+      cells[index].textContent = marker;
+    }
+  };
+
+  const resetCells = () => {
+    cells.forEach(cell => cell.textContent = "");
+  };
+
+  const highlightWin = (winPattern) => {
+    winPattern.forEach(index => {
+      cells[index].classList.add("win-cell");
+    });
+  };
+
+  const updatePlayerName = (player1, player2) => {
+    playerDivs[0].firstChild.textContent = player1.getName();
+    playerDivs[1].firstChild.textContent = player2.getName();
+  };
+
+  const bindCellListeners = (playTurn) => {
+    cells.forEach((cell, index) => {
+      cell.addEventListener("click", () => {
+        if (!gameController.isGameEnded()) {
+          const x = Math.floor(index / 3);
+          const y = index % 3;
+          updateCell(index, gameController.getCurrentPlayer().getMarker());
+          playTurn(x, y);
+        }
+      });
+    });
+  };
+
+  const bindRestart = () => {
+    restartButton.addEventListener("click", () => {
+      resetCells();
+      gameBoard.resetBoard();
+      gameController.restartGame();
+    });
+  };
+
+  const bindListeners = (playTurn) => {
+    bindCellListeners(playTurn);
+    bindRestart();
+
+    customiseButton.addEventListener("click", () => {
+      customiseDialog.showModal();
+    });
+
+    console.log("Listeners binded.");
+  };
+
+  return { bindListeners, highlightWin, updatePlayerName };
+})();
+
 const gameController = (function() {
   let currentPlayer = null;
   let gameEnded = false;
 
   const startGame = (player1, player2) => {
     currentPlayer = player1;
+    domController.updatePlayerName(player1, player2);
+    domController.bindListeners(playTurn);
     console.log(`${currentPlayer.getName()} starts the game.`);
   };
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  const isGameEnded = () => gameEnded;
+
+  const restartGame = () => gameEnded = false;
 
   const switchPlayer = () => {
     currentPlayer = currentPlayer === player1 ? player2 : player1;
@@ -52,13 +121,13 @@ const gameController = (function() {
     ];
 
     const board = gameBoard.getBoard().flat();
+
     for (const pattern of winPatterns) {
       const [a, b, c] = pattern;
       if (board[a] !== "" && board[a] === board[b] && board[b] === board[c]) {
-        console.log(a, b, c, board[a], board[b], board[c]);
         gameEnded = true;
         console.log(`${currentPlayer.getName()} wins!`);
-        return true;
+        return [a, b, c];
       }
     }
   };
@@ -80,10 +149,14 @@ const gameController = (function() {
     if (gameBoard.getBoard()[x][y] === "") {
       gameBoard.setBoard(x, y, currentPlayer.getMarker());
 
-      if (checkWin() === true) {
+      winPattern = checkWin();
+      if (winPattern) {
+        domController.highlightWin(winPattern);
         console.log(`The winner of the game is ${currentPlayer.getName()}!`);
+        return;
       } else if (checkTie() === true) {
         console.log("The game end in a tie!");
+        return;
       }
 
       switchPlayer();
@@ -94,11 +167,14 @@ const gameController = (function() {
 
   return {
     startGame,
+    getCurrentPlayer,
+    isGameEnded,
+    restartGame,
     playTurn,
   };
 })();
 
-const player1 = makeUser("Shone", "x");
-const player2 = makeUser("Sera", "o");
+const player1 = makeUser("Player 1", "x");
+const player2 = makeUser("Player 2", "o");
 
 gameController.startGame(player1, player2);
